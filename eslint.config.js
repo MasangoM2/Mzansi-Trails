8fe69bf8186c25d48fb17f7ada0104f21bb435f1
Augusto Matenja <augostomasango@gmail.com>
2025-06-15 17:52:09 +0200

    Set up ESLint with flat config and fix lint script

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// eslint.config.js
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      semi: ['error', 'always'],
+    },
+  },
+];
